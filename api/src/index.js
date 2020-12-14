702ba9986268c8de589b946f167e1003ddd58dca
konstantine899 <kostay375298918971@gmail.com>
2020-12-14 17:02:22 +0300

    request to db

--- api/src/index.js
+++ api/src/index.js
@@ -1,14 +1,32 @@
 // index.js
 const express = require('express');
+const mongoose = require('mongoose');
 const { connectDb } = require('./helpers/db');
 const { host, port, db } = require('./configuration');
 const app = express();
+
+const postSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`api сервис запущен на PORT: ${port}`);
     console.log(`api сервис запущен на HOST: ${host}`);
     console.log(`Адрес БД: ${db}`);
+
+    // Post.find(function (err, posts) {
+    //   if (err) return console.error(err);
+    //   console.log('posts', posts);
+    // });
+
+    const silence = new Post({ name: 'Silence' });
+    silence.save(function (err, savedSilence) {
+      if (err) return console.error(err);
+      console.log('savedSilence', savedSilence);
+    });
   });
 };
 
